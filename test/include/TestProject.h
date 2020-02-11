74c586298fd434b1b6a10ba597adfb7183df9b29
Lewis Hammond <s1800707@connect.glos.ac.uk>
2020-02-11 14:02:30 +0000

    Start of Seperation in to Client/Server Classes
    - Added Network Abstract Class
    - Added NetworkClient which inhertits from that class, no functionality yet

--- test/include/TestProject.h
+++ test/include/TestProject.h
@@ -4,50 +4,10 @@
 #include <stdafx.h>
 #include "Application.h"
 
-//Rak Net Includes
-#include <MessageIdentifiers.h>
-#include <RakNetTypes.h>
-
 //Forward declare
 namespace RakNet {
 	class RakPeerInterface;
 }
-
-//Connection State, not reltated to raknet
-//just to move through our own switch statement
-typedef enum ConnectionState {
-	//Client
-	CLIENT_SERVER_DECISION,
-	CLIENT_CONNECTION,
-	CLIENT_WAITING_FOR_CONNECTION,
-	CLIENT_AUTHORISATION,
-	CLIENT_WAITING_FOR_AUTHORISATION,
-	
-	//Server
-	SERVER_CLIENTS_CONNECTED,
-	SERVER_PROCESSING_EVENTS,
-	SERVER_HANDLE_CLIENT_DISCONNECT,
-
-	//Game
-	START_GAME,
-	GAME_RUNNING,
-	END_GAME,
-
-	MAX_CONNECTIONS_STATES
-
-}ConnectionState;
-
-//Custom Message Types for custom data that we are sending
-//over hte network, extend from RakNets Messages (i.e ID_CONNECTION_REQUEST_ACCEPTED)
-typedef enum CSNetMessages {
-	SERVER_AUTHENTICATE_SUCCESS = ID_USER_PACKET_ENUM + 1,
-	SERVER_AUTHENTICATE_FAIL,
-
-	CLIENT_LOGIN_DATA,
-	CLIENT_REGISTER_DATA,
-
-	CSNET_MESSAGE_END
-}CSNetMessages;
 
 // Derived application class that wraps up all globals neatly
 class TestProject : public Application
@@ -69,7 +29,6 @@ protected:
 
 	//Raknet Variables and connection status
 	void ProcessServerEvents(); //[To do client processing events]
-	RakNet::RakPeerInterface* m_pRakPeer;
 	RakNet::SystemAddress m_serverAddress;
 
 	ConnectionState m_currentState;

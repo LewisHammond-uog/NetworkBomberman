b53de11a12f18ea31a8d376ec0f4e0d5feb010f9
Lewis Hammond <s1800707@connect.glos.ac.uk>
2020-06-06 21:18:44 +0100

    Proper Colour Definitions
    Colour Defines actually work properly now

--- test/include/Colour.h
+++ test/include/Colour.h
@@ -2,32 +2,46 @@
 #define __COLOUR_H__
 
 #include "glm/glm.hpp"
-
-
 // ReSharper disable CppInconsistentNaming - For Colour Names
-// 
+
+//Type Def vec4 as a colour
+typedef glm::vec4 Colour;
+
 //Struct of predefined colours
 struct Colours
 {
 public:
-	//Type Def vec4 as a colour
-	typedef glm::vec4 Colour;
-	
 	//Primary Colours
-	Colour red = glm::vec4(1.f, 0.f, 0.f, 1.f);
-	Colour green = glm::vec4(0.f, 1.f, 0.f, 1.f);
-	Colour blue = glm::vec4(0.f, 0.f, 1.f, 1.f);
+	static const Colour red;
+	static const Colour green;
+	static const Colour blue;
 
 	//Secondsary Colours
-	Colour purple = glm::vec4(0.5f, 0.f, 0.5f, 1.f);
-	Colour orange = glm::vec4(1.0f, 0.f, 0.65f, 1.f);
-	Colour pink = glm::vec4(1.0f, 0.41f, 0.71f, 1.f);
+	static const Colour purple;
+	static const Colour orange;
+	static const Colour pink;
 
 	//Black/White
-	Colour white = glm::vec4(1.f, 1.f, 1.f, 1.f);
-	Colour black = glm::vec4(0.f, 0.f, 0.f, 1.f);
+	static const Colour white;
+	static const Colour black;
 
 };
+
+//----Static Definitions for Colours----//
+//
+//Primary Colours
+const Colour Colours::red = Colour(1.f, 0.f, 0.f, 1.f);
+const Colour Colours::green = Colour(0.f, 1.f, 0.f, 1.f);
+const Colour Colours::blue = Colour(0.f, 0.f, 1.f, 1.f);
+
+//Secondary Colours
+const Colour Colours::purple = Colour(0.5f, 0.f, 0.5f, 1.f);
+const Colour Colours::orange = Colour(1.0f, 0.f, 0.65f, 1.f);
+const Colour Colours::pink = Colour(1.0f, 0.41f, 0.71f, 1.f);
+
+//Black / White
+const Colour Colours::white = Colour(1.0f, 0.41f, 0.71f, 1.f);
+const Colour Colours::black = Colour(1.0f, 0.41f, 0.71f, 1.f);
 
 // ReSharper restore CppInconsistentNaming - For Colour Names
 

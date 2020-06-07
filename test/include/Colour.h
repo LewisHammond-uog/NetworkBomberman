1db6104dc1e62dfd7bc97645348817662b515668
Lewis Hammond <s1800707@connect.glos.ac.uk>
2020-06-07 18:45:30 +0100

    Fix Colour Definitions
    Fixed Colour Definitions so they are not mutiply defined

--- test/include/Colour.h
+++ test/include/Colour.h
@@ -4,13 +4,13 @@
 #include "glm/glm.hpp"
 // ReSharper disable CppInconsistentNaming - For Colour Names
 
-//Type Def vec4 as a colour
-typedef glm::vec4 Colour;
-
 //Struct of predefined colours
 struct Colours
 {
 public:
+	//Type Def vec4 as a colour
+	typedef glm::vec4 Colour;
+	
 	//Primary Colours
 	static const Colour red;
 	static const Colour green;
@@ -26,22 +26,7 @@ public:
 	static const Colour black;
 };
 
-//----Static Definitions for Colours----//
-//
-//Primary Colours
-const Colour Colours::red = Colour(1.f, 0.f, 0.f, 1.f);
-const Colour Colours::green = Colour(0.f, 1.f, 0.f, 1.f);
-const Colour Colours::blue = Colour(0.f, 0.f, 1.f, 1.f);
-
-//Secondary Colours
-const Colour Colours::purple = Colour(0.5f, 0.f, 0.5f, 1.f);
-const Colour Colours::orange = Colour(1.0f, 0.f, 0.65f, 1.f);
-const Colour Colours::pink = Colour(1.0f, 0.41f, 0.71f, 1.f);
-
-//Black / White
-const Colour Colours::white = Colour(1.0f, 0.41f, 0.71f, 1.f);
-const Colour Colours::black = Colour(1.0f, 0.41f, 0.71f, 1.f);
-
 // ReSharper restore CppInconsistentNaming - For Colour Names
+
 
 #endif //!__COLOUR_H__
---
+++ test/src/Colours.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "Colour.h"
+
+//----Static Definitions for Colours----//
+//
+//Primary Colours
+const glm::vec4 Colours::red = Colour(1.f, 0.f, 0.f, 1.f);
+const glm::vec4 Colours::green = Colour(0.f, 1.f, 0.f, 1.f);
+const glm::vec4 Colours::blue = Colour(0.f, 0.f, 1.f, 1.f);
+
+//Secondary Colours
+const glm::vec4 Colours::purple = Colour(0.5f, 0.f, 0.5f, 1.f);
+const glm::vec4 Colours::orange = Colour(1.0f, 0.f, 0.65f, 1.f);
+const glm::vec4 Colours::pink = Colour(1.0f, 0.41f, 0.71f, 1.f);
+
+//Black / White
+const glm::vec4 Colours::white = Colour(1.0f, 0.41f, 0.71f, 1.f);
+const glm::vec4 Colours::black = Colour(1.0f, 0.41f, 0.71f, 1.f);
+
+
+//--------------------------------------//
